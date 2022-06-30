50b735d6a9f31e7648ebea4d6c2b85698c1d1353
Samson Dada <samsondada09@gmail.com>
2022-06-30 03:40:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,194 @@
+const staffDataTable = {
+  serialNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  firstName: ["Thompson", "Johnson", "Chisom", "Jide", "Williams", "Nancy", "Nicholas", "Ahmed", "Halima", "Richard"],
+  lastName: ["Anthony", "Mary", "Onyeka", "Olabisi", "Ajayi", "Chukwuemeka", "Uchemba", "Olayinka", "Mohammed", "Adeyemi"],
+  age: [32, 23, 28, 29, 30, 27, 26, 24, 26, 41],
+  sex: ["Male", "Female", "Female", "Male", "Male", "Female", "Male", "Male", "Female", "Male"],
+  position: [
+    "Head of Finance Department",
+    "Sofware Enginner",
+    "Human Resource Manager",
+    "Assitant Head(Sofware Dapartment)",
+    "Head of Software Department",
+    "Secretary",
+    "Sofware Enginner",
+    "Graphic Designer",
+    "Senior Finance Manager",
+    "C.E.0",
+  ],
+  dateEmployed: [
+    "17/07/2014",
+    "05/03/2019",
+    "21/06/2016",
+    "05/06/2016",
+    "06/09/2015",
+    "23/08/2016",
+    "22/02/2020",
+    "15/08/2020",
+    "30/05/2018",
+    "17/011/2010",
+  ],
+};
+
+// TABLE serial number
+
+const staffOneNum = document.getElementById("staff__one-number");
+staffOneNum.innerHTML = staffDataTable.serialNumber[0];
+const staffTwoNum = document.getElementById("staff__two-number");
+staffTwoNum.innerHTML = staffDataTable.serialNumber[1];
+const staffThreeNum = document.getElementById("staff__three-number");
+staffThreeNum.innerHTML = staffDataTable.serialNumber[2];
+const staffFourNum = document.getElementById("staff__four-number");
+staffFourNum.innerHTML = staffDataTable.serialNumber[3];
+
+const staffFiveNum = document.getElementById("staff__five-number");
+staffFiveNum.innerHTML = staffDataTable.serialNumber[4];
+const staffSixNum = document.getElementById("staff__six-number");
+staffSixNum.innerHTML = staffDataTable.serialNumber[5];
+const staffSevenNum = document.getElementById("staff__seven-number");
+staffSevenNum.innerHTML = staffDataTable.serialNumber[6];
+const staffEightNum = document.getElementById("staff__eight-number");
+staffEightNum.innerHTML = staffDataTable.serialNumber[7];
+const staffNineNum = document.getElementById("staff__nine-number");
+staffNineNum.innerHTML = staffDataTable.serialNumber[8];
+const staffTenNum = document.getElementById("staff__ten-number");
+staffTenNum.innerHTML = staffDataTable.serialNumber[9];
+
+// TABLE staff one
+
+const staffOneFirstName = document.getElementById("staff__one-firstname");
+staffOneFirstName.innerHTML = staffDataTable.firstName[0];
+const staffOneLastName = document.getElementById("staff__one-lastname");
+staffOneLastName.innerHTML = staffDataTable.lastName[0];
+const staffOneAge = document.getElementById("staff__one-age");
+staffOneAge.innerHTML = staffDataTable.age[0];
+const staffOneSex = document.getElementById("staff__one-sex");
+staffOneSex.innerHTML = staffDataTable.sex[0];
+const staffOnePosition = document.getElementById("staff__one-position");
+staffOnePosition.innerHTML = staffDataTable.position[0];
+const staffOneDateEmployed = document.getElementById("staff__one-date");
+staffOneDateEmployed.innerHTML = staffDataTable.dateEmployed[0];
+
+// TABLE staff two
+const staffTwoFirstName = document.getElementById("staff__two-firstname");
+staffTwoFirstName.innerHTML = staffDataTable.firstName[1];
+const staffTwoLastName = document.getElementById("staff__two-lastname");
+staffTwoLastName.innerHTML = staffDataTable.lastName[1];
+const staffTwoAge = document.getElementById("staff__two-age");
+staffTwoAge.innerHTML = staffDataTable.age[1];
+const staffTwoSex = document.getElementById("staff__two-sex");
+staffTwoSex.innerHTML = staffDataTable.sex[1];
+const staffTwoPosition = document.getElementById("staff__two-position");
+staffTwoPosition.innerHTML = staffDataTable.position[1];
+const staffTwoDateEmployed = document.getElementById("staff__two-date");
+staffTwoDateEmployed.innerHTML = staffDataTable.dateEmployed[1];
+// TABLE staff three
+const staffThreeFirstName = document.getElementById("staff__three-firstname");
+staffThreeFirstName.innerHTML = staffDataTable.firstName[2];
+const staffThreeLastName = document.getElementById("staff__three-lastname");
+staffThreeLastName.innerHTML = staffDataTable.lastName[2];
+const staffThreeAge = document.getElementById("staff__three-age");
+staffThreeAge.innerHTML = staffDataTable.age[2];
+const staffThreeSex = document.getElementById("staff__three-sex");
+staffThreeSex.innerHTML = staffDataTable.sex[2];
+const staffThreePosition = document.getElementById("staff__three-position");
+staffThreePosition.innerHTML = staffDataTable.position[2];
+const staffThreeDateEmployed = document.getElementById("staff__three-date");
+staffThreeDateEmployed.innerHTML = staffDataTable.dateEmployed[2];
+
+// TABLE staff four
+const staffFourFirstName = document.getElementById("staff__four-firstname");
+staffFourFirstName.innerHTML = staffDataTable.firstName[3];
+const staffFourLastName = document.getElementById("staff__four-lastname");
+staffFourLastName.innerHTML = staffDataTable.lastName[3];
+const staffFourAge = document.getElementById("staff__four-age");
+staffFourAge.innerHTML = staffDataTable.age[3];
+const staffFourSex = document.getElementById("staff__four-sex");
+staffFourSex.innerHTML = staffDataTable.sex[3];
+const staffFourPosition = document.getElementById("staff__four-position");
+staffFourPosition.innerHTML = staffDataTable.position[3];
+const staffFourDateEmployed = document.getElementById("staff__four-date");
+staffFourDateEmployed.innerHTML = staffDataTable.dateEmployed[3];
+// TABLE staff five
+const staffFiveFirstName = document.getElementById("staff__five-firstname");
+staffFiveFirstName.innerHTML = staffDataTable.firstName[4];
+const staffFiveLastName = document.getElementById("staff__five-lastname");
+staffFiveLastName.innerHTML = staffDataTable.lastName[4];
+const staffFiveAge = document.getElementById("staff__five-age");
+staffFiveAge.innerHTML = staffDataTable.age[4];
+const staffFiveSex = document.getElementById("staff__five-sex");
+staffFiveSex.innerHTML = staffDataTable.sex[4];
+const staffFivePosition = document.getElementById("staff__five-position");
+staffFivePosition.innerHTML = staffDataTable.position[4];
+const staffFiveDateEmployed = document.getElementById("staff__five-date");
+staffFiveDateEmployed.innerHTML = staffDataTable.dateEmployed[4];
+
+// TABLE staff six
+const staffSixFirstName = document.getElementById("staff__six-firstname");
+staffSixFirstName.innerHTML = staffDataTable.firstName[5];
+const staffSixLastName = document.getElementById("staff__six-lastname");
+staffSixLastName.innerHTML = staffDataTable.lastName[5];
+const staffSixAge = document.getElementById("staff__six-age");
+staffSixAge.innerHTML = staffDataTable.age[5];
+const staffSixSex = document.getElementById("staff__six-sex");
+staffSixSex.innerHTML = staffDataTable.sex[5];
+const staffSixPosition = document.getElementById("staff__six-position");
+staffSixPosition.innerHTML = staffDataTable.position[5];
+const staffSixDateEmployed = document.getElementById("staff__six-date");
+staffSixDateEmployed.innerHTML = staffDataTable.dateEmployed[5];
+
+// TABLE staff seven
+const staffSevenFirstName = document.getElementById("staff__seven-firstname");
+staffSevenFirstName.innerHTML = staffDataTable.firstName[6];
+const staffSevenLastName = document.getElementById("staff__seven-lastname");
+staffSevenLastName.innerHTML = staffDataTable.lastName[6];
+const staffSevenAge = document.getElementById("staff__seven-age");
+staffSevenAge.innerHTML = staffDataTable.age[6];
+const staffSevenSex = document.getElementById("staff__seven-sex");
+staffSevenSex.innerHTML = staffDataTable.sex[6];
+const staffSevenPosition = document.getElementById("staff__seven-position");
+staffSevenPosition.innerHTML = staffDataTable.position[6];
+const staffSevenDateEmployed = document.getElementById("staff__seven-date");
+staffSevenDateEmployed.innerHTML = staffDataTable.dateEmployed[6];
+
+// TABLE staff eight
+const staffEightFirstName = document.getElementById("staff__eight-firstname");
+staffEightFirstName.innerHTML = staffDataTable.firstName[7];
+const staffEightLastName = document.getElementById("staff__eight-lastname");
+staffEightLastName.innerHTML = staffDataTable.lastName[7];
+const staffEightAge = document.getElementById("staff__eight-age");
+staffEightAge.innerHTML = staffDataTable.age[7];
+const staffEightSex = document.getElementById("staff__eight-sex");
+staffEightSex.innerHTML = staffDataTable.sex[7];
+const staffEightPosition = document.getElementById("staff__eight-position");
+staffEightPosition.innerHTML = staffDataTable.position[7];
+const staffEightDateEmployed = document.getElementById("staff__eight-date");
+staffEightDateEmployed.innerHTML = staffDataTable.dateEmployed[7];
+
+// TABLE staff nine
+const staffNineFirstName = document.getElementById("staff__nine-firstname");
+staffNineFirstName.innerHTML = staffDataTable.firstName[8];
+const staffNineLastName = document.getElementById("staff__nine-lastname");
+staffNineLastName.innerHTML = staffDataTable.lastName[8];
+const staffNineAge = document.getElementById("staff__nine-age");
+staffNineAge.innerHTML = staffDataTable.age[8];
+const staffNineSex = document.getElementById("staff__nine-sex");
+staffNineSex.innerHTML = staffDataTable.sex[8];
+const staffNinePosition = document.getElementById("staff__nine-position");
+staffNinePosition.innerHTML = staffDataTable.position[8];
+const staffNineDateEmployed = document.getElementById("staff__nine-date");
+staffNineDateEmployed.innerHTML = staffDataTable.dateEmployed[8];
+
+// TABLE staff nine
+const staffTenFirstName = document.getElementById("staff__ten-firstname");
+staffTenFirstName.innerHTML = staffDataTable.firstName[9];
+const staffTenLastName = document.getElementById("staff__ten-lastname");
+staffTenLastName.innerHTML = staffDataTable.lastName[9];
+const staffTenAge = document.getElementById("staff__ten-age");
+staffTenAge.innerHTML = staffDataTable.age[9];
+const staffTenSex = document.getElementById("staff__ten-sex");
+staffTenSex.innerHTML = staffDataTable.sex[9];
+const staffTenPosition = document.getElementById("staff__ten-position");
+staffTenPosition.innerHTML = staffDataTable.position[9];
+const staffTenDateEmployed = document.getElementById("staff__ten-date");
+staffTenDateEmployed.innerHTML = staffDataTable.dateEmployed[9];
